ee4776c3497942958087ba47e44e9af845d25774
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-05-18 21:11:48 -0400

    update

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 let buttonAdd = document.querySelector('#add');
 let buttonRemove = document.querySelector('#remove');
 let input = document.querySelector('#input');
+let star = document.querySelector('#star')
+let count = 0;
 
 buttonAdd.addEventListener('click', () => {
 	input.value = parseInt(input.value) + 1;
+	addStar();
 });
 
 buttonRemove.addEventListener('click', () => {
 	input.value = parseInt(input.value) - 1;
+	if(count > 0){
+		count -= 1;
+		input.value = count;
+		removeStar();
+	}
 });
+
+const addStar = () => {
+	const starDiv = document.createElement("div");
+	starDiv.id = "starDiv";
+	const image = document.createTextNode("*");
+	starDiv.appendChild(image);
+	star.appendChild(starDiv);
+}
+
+const removeStar = () => {
+	let lastStar = document.querySelector("#star :last-of-type");
+	star.removeChild(lastStar);
+}
